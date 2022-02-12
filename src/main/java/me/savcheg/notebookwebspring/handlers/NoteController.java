e30dcae4bc0c3aa556ec70ec08fb47b5d8569b9a
savcheg <savcheg.de@yandex.ru>
2022-02-12 13:51:29 +0300

    modified NoteController.java: added Get and Post for Note

--- src/main/java/me/savcheg/notebookwebspring/handlers/NoteController.java
+++ src/main/java/me/savcheg/notebookwebspring/handlers/NoteController.java
@@ -1,4 +1,26 @@
 package me.savcheg.notebookwebspring.handlers;
 
+import me.savcheg.notebookwebspring.entities.Note;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class NoteController {
+    List<Note> notes = new ArrayList<>();
+
+    @GetMapping("/notes")
+    public List<Note> getNotes(){
+        return notes;
+    }
+
+    @PostMapping("/newnote")
+    public ResponseEntity<String> postNote(@RequestBody Note note){
+        notes.add(note);
+        return new ResponseEntity<>("Note added" ,HttpStatus.OK);
+    }
 }
