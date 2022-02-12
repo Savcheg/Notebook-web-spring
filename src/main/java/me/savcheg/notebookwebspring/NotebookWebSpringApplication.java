9220fcf2c0ba83429d44438727b4931d48b7e118
savcheg <savcheg.de@yandex.ru>
2022-02-12 13:40:49 +0300

    Initial commit

---
+++ src/main/java/me/savcheg/notebookwebspring/NotebookWebSpringApplication.java
@@ -0,0 +1,13 @@
+package me.savcheg.notebookwebspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookWebSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookWebSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/savcheg/notebookwebspring/NotebookWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.savcheg.notebookwebspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookWebSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
